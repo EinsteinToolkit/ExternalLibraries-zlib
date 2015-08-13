dc4a17ac84512c0b343031cb473523594f3d835b
eschnett <eschnett@d47fded2-66c1-406f-af09-4fc0800f9c6b>
2015-08-13 15:59:40 +0000

    Support Cygwin
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/zlib/trunk@51 d47fded2-66c1-406f-af09-4fc0800f9c6b

--- configure.sh
+++ configure.sh
@@ -25,7 +25,7 @@ if [ -z "${ZLIB_DIR}" ]; then
     # look into each directory
     for dir in $DIRS; do
         # libraries might have different file extensions
-        for libext in a so dylib; do
+        for libext in a dll dll.a dylib so; do
             # libraries can be in /lib or /lib64
             for libdir in lib64 lib/x86_64-linux-gnu lib lib/i386-linux-gnu lib/arm-linux-gnueabihf; do
                 FILES="include/zlib.h $libdir/libz.$libext"
